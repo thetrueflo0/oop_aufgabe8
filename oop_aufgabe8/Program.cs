678b705087e452248b526076881601cc199a9ceb
fca124419 <fca124419@stud.gibb.ch>
2023-09-14 18:06:57 +0200

    Projektdateien hinzufügen.

---
+++ oop_aufgabe8/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tsbe.CodeCracker;
+namespace oop_aufgabe8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hash= Tsbe.CodeCracker.MD5Hash.GeneratePassword("adb");
+            Console.WriteLine(hash);
+
+            Tsbe.CodeCracker.MD5Hash.CrackPassword(hash, 3);
+            MD5Hash.CrackPassword(hash, 3);
+        }
+    }
+}
